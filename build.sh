a8338f71de4ee5ef53ba55b3ed253a67faf46134
Wade Robson <wade.robson4@gmail.com>
2018-05-16 23:40:13 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@ MUNKI_DIR=/usr/local/munki
 mkdir -p {ROOT/usr/local/munki/,build}
 cp middleware_s3.py ROOT/${MUNKI_DIR}/
 
-if [ ! -z $TRAVIS_TAG ]; then
-VERSION=$(echo $TRAVIS_TAG | cut -c2-)
+VERSION=1.2
   echo "building package $VERSION"
   pkgbuild --root ROOT --identifier com.github.s3-auth \
   --version $VERSION build/com.github.s3-auth.pkg
-fi
