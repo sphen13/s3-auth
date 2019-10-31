ea52f622970b2c9dc973d10c39e1344ab39b3b99
Stephen Boyle <stephen@thirdvantage.com>
2019-10-31 16:47:35 -0400

    Update for python3

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ MUNKI_DIR=/usr/local/munki
 mkdir -p {ROOT/usr/local/munki/,build}
 cp middleware_s3.py ROOT/${MUNKI_DIR}/
 
-VERSION=1.2
+VERSION=1.3
   echo "building package $VERSION"
   pkgbuild --root ROOT --identifier com.github.s3-auth \
   --version $VERSION build/com.github.s3-auth.pkg
